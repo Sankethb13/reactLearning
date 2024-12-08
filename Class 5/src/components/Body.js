625cf099b99dccffbcf061da55bdee1e3a939bf8
sanketh bachoti <srigandha.eleswarapu@gmail.com>
2024-12-09 00:59:31 +0530

    Changes for class 6

--- Class 5/src/components/Body.js
+++ Class 5/src/components/Body.js
@@ -1,7 +1,32 @@
 import RestaurantCard from './RestaurantCard';
-import resObj from '../../utils/mockData';
+import { useState, useEffect } from 'react';
+import Shimmer from './Shimmer';
 
 const Body = () => {
+  const [listOfRestarurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+    );
+    console.log('Complete data', data);
+    const json = await data.json(); // convert the data to json
+    console.log('format data', json);
+    console.log(
+      'format data 1',
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setListOfRestaurants(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+  if (listOfRestarurants.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
@@ -9,13 +34,17 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             console.log('button clicked');
+            const filteredList = listOfRestarurants.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setListOfRestaurants(filteredList);
           }}
         >
           Top Rated Button
         </button>
       </div>
       <div className="res-container">
-        {resObj.map((restaurant) => (
+        {listOfRestarurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
---
+++ Class 6/readme.md
@@ -0,0 +1,21 @@
+### Exploring the World:
+
+Discussed on Monolith and Micorservice Architecture:
+
+Using react we follow below approach:
+
+Page Loads -> Renders UI -> Calls API to fetch the data to display -> Re-Render again
+
+- React Renders cycles are very fast. No need to bother much about how many times we are rendering
+
+## Hook:
+It is nothing but a normal JS function. Which is provided by react. 
+## UseEffect Hook:
+Its a react library. It is called like a function. It has two arguments, first one is a call back function, second one is dependency array.
+When is this useEffect call back function is called, It is called when the component renders. 
+
+## Cors Policy:
+Our browsers does not allow swiggy API from Local host. Means it does not allow other API from the localhost
+
+## Shimmer UI:
+Resembles the page like actual UI, so users will understand how quickly the page is loaded.
